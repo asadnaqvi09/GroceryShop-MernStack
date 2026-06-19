88517eb7e69735c077f31733cb924d708fcdaa56
asadnaqvi08 <156691032+asadnaqvi09@users.noreply.github.com>
2026-06-19 23:18:37 +0500

    Final Steps Fix Deploy Issues

--- Project-Server/index.js
+++ Project-Server/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
